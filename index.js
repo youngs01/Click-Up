eddcf8f18229b4e5a36e3fb7b508e9ea8ad807a1
youngs01 <youngscorporations0319@gmail.com>
2024-01-12 04:29:47 -0800

    hi

---
+++ index.js
@@ -0,0 +1,15 @@
+function dropDown() {
+     productContent = document.getElementById("productContent");
+    productContent.style.display = (productContent.style.display === "none" || productContent.style.display === "") ? "block" : "none";
+  }
+
+
+
+
+
+
+
+
+
+
+
